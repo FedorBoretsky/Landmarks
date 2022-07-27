866461b9633d1e18290924b19a626aa29c61bfcd
Fedor Boretsky <fedor.boretsky@gmail.com>
2022-07-27 10:42:37 +0300

    Building Lists and Navigation – 4 Create the List of Landmarks.

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Fedor Boretsky on 27.07.2022.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
